830c50b4d79b22cc63264858caf4c1392ed8b34b
joshuaas5 <joshuaas500@gmail.com>
2026-04-22 16:46:50 -0300

    chore: remove temporary fix scripts from root

--- fix_api.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-console.error('[deprecated] fix_api.js foi desativado por segurança. Use scripts versionados com testes.');
-process.exit(1);
--- fix_brutalist.js
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-
-console.error('[deprecated] fix_brutalist.js foi desativado por segurança. Use alterações manuais com revisão de diff.');
-process.exit(1);
